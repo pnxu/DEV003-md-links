2fecd76f496970b790441a679ecfb3301363af0a
Francisca V <fcaavv@gmail.com>
2023-03-18 20:18:50 -0300

    feat(unit-tests): first test file

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,27 @@
-const mdLinks = require('../');
-
+const {mdLinks} = require('.././src/md-links.js');
+const {isAbsolutePath}  = require('../src/functions.js')
 
 describe('mdLinks', () => {
 
   it('should...', () => {
     console.log('FIX ME!');
   });
-
+  // it('Debe devolver una promesa', () => {
+  //   expect(mdLinks()).toBe(typeof Promise);
+  // });
+  it('Debe rechazar cuando el path no existe', () => {
+    return mdLinks('pnxu/workspace/noexiste.md').catch((error) => {
+      expect(error).toBe('La ruta no existe')
+  })
+});
+});
+describe('isAbsolutePath', () => {
+  it('debe retornar true para rutas absolutas', () => {
+    const absolutePath = '/DEV003-MD-LINKS/carpeta/archivotxt.txt';
+    expect(isAbsolutePath(absolutePath)).toBe(true);
+  });
+});
+it('should return false for relative paths', () => {
+  const relativePath = './carpeta/archivotxt.txt';
+  expect(isAbsolutePath(relativePath)).toBe(false);
 });
