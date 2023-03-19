e17fb7d42baaf95ba51d1d412a381ca0cff44fd2
Francisca V <fcaavv@gmail.com>
2023-03-19 18:02:32 -0300

    chore: rename functions.js to utils.js.  translate comments in utils

--- src/utils.js
+++ src/utils.js
@@ -3,20 +3,23 @@ const fs = require('fs');
 // const marked = require("marked");
 // const markdownLinkExtractor = require('markdown-link-extractor');
 // const axios = require('axios');
- 
 
 // Check synchronously if given path exists.
 const pathExists = (userPath) => fs.existsSync(userPath);
 // console.log(pathExists('/README.md'));
 
-// Check if given p ath is an absolute route.
+// Check if given path is an absolute route.
 // return Boolean
 const isAbsolutePath = (userPath) => path.isAbsolute(userPath);
 // console.log(isAbsolutePath('README.md'));
 
 // Get the absolute path from a given relative path.
 // return String
-const resolveToAbsolutePath = (userPath) => path.resolve(userPath);
+const resolveToAbsolutePath = (userPath) => {
+  const result = path.resolve(userPath);
+  console.log('resolving path', userPath, result);
+  return result;
+};
 // console.log(resolveToAbsolutePath('README.md'));
 
 // Get the file stats and check if the target is a file.
@@ -31,15 +34,16 @@ const isDirectory = (userPath) => fs.lstatSync(userPath).isDirectory();
 
 // Get the file of a given path, and check if the extension is .md.
 // return Boolean
-const fileIsMarkdown = (userPath) => (path.extname(userPath) === '.md' );
+const fileIsMarkdown = (userPath) => path.extname(userPath) === '.md';
 // console.log(getExtension('/home/pnxu/workspace/DEV003-md-links/README.js'))
 
 // Read a file synchronously
 // return Buffer
 // TODO:  - Change to async read
-//        - DRY dont repeat yourself (dont re-declare functions that do the exact same thing)
+//        - DRY dont repeat yourself (dont re-declare functions that
+//  do the exact same thing
 const readFileSync = (userPath) => fs.readFileSync(userPath, 'utf-8');
-console.log(readFileSync('README.md'))
+// console.log(readFileSync('README.md'));
 
 // Read the directory content
 const readDirectory = (userPath) => fs.readdirSync(userPath);
@@ -47,33 +51,33 @@ const readDirectory = (userPath) => fs.readdirSync(userPath);
 
 // Check if the directory of a given path contain files
 const isThereFiles = (userPath) => {
-    if(fs.readdirSync(userPath).length > 0) {
-        return true;
-    } else {
-        return false;
-    }
+  if (fs.readdirSync(userPath).length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 // console.log(isThereFiles('/home/pnxu/workspace/DEV003-md-links/'))
 
-// This function reads files in a directory and their subdirectories recursively, and filter the .md files.
+// This function reads files in a directory and their subdirectories
+//  recursively, and filter the .md files.
 // It returns an array of paths.
 const getMdFilesInDirectory = (userPath) => {
-    const files = fs.readdirSync(userPath);
-    const mdFiles = files.flatMap((file) => {
-      const filePath = path.join(userPath, file);
-      const stat = fs.statSync(filePath);
-      const isDirectory = stat.isDirectory()
-      if (isDirectory) {
-        return getMdFilesInDirectory(filePath);
-      } else if (stat.isFile() && path.extname(file) === ".md") {
-        return filePath;
-      } else {
-        return [];
-      }
-    });
-    return mdFiles;
-  };  
-console.log(getMdFilesInDirectory('/home/pnxu/workspace/DEV003-md-links'));
+  const files = fs.readdirSync(userPath);
+  const mdFiles = files.flatMap((file) => {
+    const filePath = path.join(userPath, file);
+    const stat = fs.statSync(filePath);
+    const isDirectory = stat.isDirectory();
+    if (isDirectory) {
+      return getMdFilesInDirectory(filePath);
+    } else if (stat.isFile() && path.extname(file) === '.md') {
+      return filePath;
+    } else {
+      return [];
+    }
+  });
+  return mdFiles;
+};
 
 // get links
 // const getLinks = (userPath) => {
@@ -112,15 +116,16 @@ console.log(getMdFilesInDirectory('/home/pnxu/workspace/DEV003-md-links'));
 //     return links;
 //   };
 // console.log(getLinks('/home/pnxu/workspace/DEV003-md-links/carpeta/'));
-  
+
 module.exports = {
-    pathExists,
-    isAbsolutePath,
-    resolveToAbsolutePath,
-    isFile,
-    isDirectory,
-    fileIsMarkdown,
-    readFileSync,
-    readDirectory,
-    getMdFilesInDirectory,
+  pathExists,
+  isAbsolutePath,
+  resolveToAbsolutePath,
+  isFile,
+  isDirectory,
+  fileIsMarkdown,
+  readFileSync,
+  readDirectory,
+  isThereFiles,
+  getMdFilesInDirectory
 };
