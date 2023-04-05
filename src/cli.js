339aa6fe77d768e22f36c6951a324b1762a5a73b
Francisca V <fcaavv@gmail.com>
2023-04-04 21:27:56 -0400

    feat(cli): run cli with validation flag

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,17 @@
 const {mdLinks} = require('./md-links.js');
 
-mdLinks('../archivo.md').then(() => {
-})
-.catch((error) => {
-    console.log(error)
-});
+// const input = process.argv;
+const route = process.argv[2];
+const utils = require('./utils.js')
+const validate = process.argv.includes('--validate');
+const stats = process.argv.includes('--stats');
+
+if (validate) {
+  mdLinks(route, {validate: true})
+    .then((result) => console.log('result', result))
+    .catch((err) => console.error(err));
+} else {
+  mdLinks(route, {validate: false})
+    .then((result) => console.log('result', result))
+    .catch((err) => console.error(err));
+}
